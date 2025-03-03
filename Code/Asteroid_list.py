ffc4b74ddc704f621fe8726d435c8362336f9549
Zeke DeLaughter <zeke.delaughter@gmail.com>
2025-03-03 09:13:31 -0700

    Added asteroid object and code to check lists and each individually

--- Code/Asteroid_list.py
+++ Code/Asteroid_list.py
@@ -5,6 +5,8 @@ import requests
 import pandas as pd
 import re
 
+from indiv_ast import Asteroid
+
 url = "https://ssd.jpl.nasa.gov/api/horizons.api"
 
 
@@ -80,14 +82,67 @@ def gen_panda(list, header):
     return df
 
 
+def indiv_params(input):
+    parameters = {}
+    file = open(input, 'r')
+    start = "$"
+    stop = "$$"
+    check = False
+    for lines in file:
+        if stop in lines:
+            break
+        if check:
+            line = lines.split(":")
+            parameters[line[0]] = line[1].strip("\n")
+        if start in lines:
+            check = True
+    return parameters
+
+
+def search_asts(df,params):
+    print("There are {len} asteroids in the list".format(len=len(df)))
+    count = int(input("How many asteroids would you like to query: "))
+    i = 0
+    while True:
+        while i < count:
+            for desig in df["Primary Desig"].head(count):
+                ast = Asteroid(desig,df["Name"][i])
+                ast.set_params(params)
+                response = requests.get(url,data=ast.params)
+                data = response.json()
+                ast.get_ephem(data)
+                ast.get_header(data)
+                ast.set_df()  
+                eval_df(ast)
+                i += 1
+        answer = input("Would you like to observe more asteroid info? Y/N: ")
+        if answer == "Y":
+            count += int(input("How many more asteroids would you like to observe: "))
+        else:
+            break
+
+
+def eval_df(ast):
+    print("Values on {date} for asteroid {name}:".format(date=ast.df["Date__(UT)__HR:MN"][0],name=ast.name))
+    df = ast.df.apply(pd.to_numeric, errors='coerce')
+    means = df.mean()
+    for col,value in means.items():
+        print("{col}: {value}".format(col=col,value=value)+'\n')
+
+
+
 def main():
+    
     list_params = get_list_params("Input_Parameters.txt")
     list_response = requests.get(url, params=list_params)
     list_data = list_response.json()
     list, header = get_lists(list_data)
+
+    params = indiv_params("Input_Parameters.txt")
     df = gen_panda(list, header)
-    df.to_csv("Asteroid_List.csv", index=False)
-    write_list_data("Asteroid_List.txt", list_data)
+    search_asts(df, params)
+    #df.to_csv("Asteroid_List.csv", index=False)
+    #write_list_data("Asteroid_List.txt", list_data)
 
 if __name__ == "__main__":
     main()
---
+++ Code/indiv_ast.py
@@ -0,0 +1,54 @@
+import re
+import pandas as pd
+
+
+class Asteroid:
+    def __init__(self,desig,name):
+        self.desig = desig
+        self.name = name
+        self.params = {}
+        self.ephem = []
+        self.header = []
+        self.df = None
+    
+    def get_ephem(self, data):
+        results = data['result']
+        lines = results.split("\n")
+        lock = False
+        start = "$$SOE"; end = "$$EOE"
+        for line in lines:
+            if lock:
+                self.ephem.append(line)
+            if start in line:
+                lock = True
+            if end in line:
+                lock = False
+        self.ephem.pop(-1)
+
+    def get_header(self, data):
+        results = data['result']
+        lines = results.split("\n")
+        i = 0
+        check = "$$SOE"
+        while i < len(results):
+            if check in lines[i]:
+                start = i-2
+                break
+            i += 1
+        self.header = re.split(r"\s+",lines[start:i-1][0])
+        self.header.pop(0)
+    
+    def set_params(self,params):
+        params['COMMAND'] = "'NAME={f.desig}'".format(f=self)
+        self.params = params
+
+    def set_df(self):
+        data = {}
+        i = 0
+        while i < len(self.ephem):
+            line = re.split(r"\s{2,}", self.ephem[i])
+            self.ephem[i] = line
+            i += 1
+        for i in range(len(self.header)):
+            data[self.header[i]] = [line[i] for line in self.ephem]
+        self.df = pd.DataFrame(data)
