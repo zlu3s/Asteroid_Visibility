29ca0aefabb68b7091538aeeeb5f6a4c0a391501
Zeke DeLaughter <zeke.delaughter@gmail.com>
2025-03-21 16:36:01 -0700

    Added get_sun into Asteroid_list, and added location list

--- Code/Asteroid_list.py
+++ Code/Asteroid_list.py
@@ -7,7 +7,15 @@ import re
 
 from indiv_ast import Asteroid
 
+from skyfield.api import load, Topos
+from skyfield.almanac import find_discrete, sunrise_sunset
+
 url = "https://ssd.jpl.nasa.gov/api/horizons.api"
+
+locations = {
+    "Tucson": Topos(latitude_degrees=32.2226, longitude_degrees=-110.9747),
+    "UKIRT": Topos(latitude_degrees=19.825, longitude_degrees=-155.4717, elevation_m=4192)
+}
 
 
 def get_list_params(input):
@@ -28,13 +36,6 @@ def get_list_params(input):
 
 
 
-def write_list_data(output, data):
-    file = open(output, "w")
-    for k,v in data.items():
-        file.write(f"{k}: {v}\n")
-
-
-
 def get_lists(data):
     results = data['result']
     list = []
@@ -51,6 +52,8 @@ def get_lists(data):
             list.append(line)
     list,header = clean(list)
     return list, header
+
+
 
 def clean(list):
     """
@@ -73,6 +76,7 @@ def clean(list):
     return list,header
 
 
+
 def gen_panda(list, header):
     data = {}
     # just takes the first 4 parameters for now (can be updated later)
@@ -80,6 +84,7 @@ def gen_panda(list, header):
         data[header[i]] = [line[i] for line in list]
     df = pd.DataFrame(data)
     return df
+
 
 
 def indiv_params(input):
@@ -96,53 +101,85 @@ def indiv_params(input):
             parameters[line[0]] = line[1].strip("\n")
         if start in lines:
             check = True
+
+    if parameters["CENTER"] == "UKIRT":
+        parameters["CENTER"] = "V38"
+    elif parameters["CENTER"] == "TUCSON":
+        parameters["CENTER"] = "G37"
+
     return parameters
 
 
-def search_asts(df,params):
-    print("There are {len} asteroids in the list".format(len=len(df)))
+
+def search_asts(df, params):
+    print("There are {} asteroids in the list".format(len(df)))
+    
     count = int(input("How many asteroids would you like to query: "))
-    i = 0
+    
     while True:
-        while i < count:
-            for desig in df["Primary Desig"].head(count):
-                ast = Asteroid(desig,df["Name"][i])
-                ast.set_params(params)
-                response = requests.get(url,data=ast.params)
-                data = response.json()
-                ast.get_ephem(data)
-                ast.get_header(data)
-                ast.set_df()  
-                eval_df(ast)
-                i += 1
-        answer = input("Would you like to observe more asteroid info? Y/N: ")
+        for i, desig in enumerate(df["Primary Desig"].head(count)):
+            ast = Asteroid(desig, df["Name"].iloc[i])  # Use iloc for indexing
+            ast.set_params(params)
+            response = requests.get(url, data=ast.params)
+            data = response.json()
+            ast.get_ephem(data)
+            ast.get_header(data)
+            ast.set_df()  
+            print(ast.df)
+        answer = input("Would you like to observe more asteroid info? (Y/N): ").strip().upper()
         if answer == "Y":
-            count += int(input("How many more asteroids would you like to observe: "))
+            more = input("How many more asteroids would you like to observe: ")
+            if more.isdigit():  # Ensure valid input
+                count += int(more)
+            else:
+                print("Invalid input, exiting.")
+                break
         else:
             break
 
 
-def eval_df(ast):
-    print("Values on {date} for asteroid {name}:".format(date=ast.df["Date__(UT)__HR:MN"][0],name=ast.name))
-    df = ast.df.apply(pd.to_numeric, errors='coerce')
-    means = df.mean()
-    for col,value in means.items():
-        print("{col}: {value}".format(col=col,value=value)+'\n')
+
+def get_sun(params):
+    # Load ephemeris
+    eph = load('de421.bsp')
+    ts = load.timescale()
+    times = {}
+
+    # Define location (Tucson, AZ)
+    location = Topos(latitude_degrees=32.2226, longitude_degrees=-110.9747)
+
+    # Define time range
+    start_time = ts.utc(2025, 3, 19)
+    end_time = ts.utc(2025, 3, 20)
+
+    # Get the sunrise and sunset function
+    f = sunrise_sunset(eph, location)
+
+    # Find discrete events (sunrise, sunset) within the time range
+    times, events = find_discrete(start_time, end_time, f)
+
+    # Print results
+    for t, e in zip(times, events):
+        event_name = "Sunrise" if e == 1 else "Sunset"
+        times[event_name] = t.utc_datetime()
+    return times
 
 
 
 def main():
     
-    list_params = get_list_params("Input_Parameters.txt")
+    list_params = get_list_params("C:/Users/Research/Asteroid Project/in_out/Input_Parameters.txt")
     list_response = requests.get(url, params=list_params)
     list_data = list_response.json()
     list, header = get_lists(list_data)
 
-    params = indiv_params("Input_Parameters.txt")
+    params = indiv_params("C:/Users/Research/Asteroid Project/in_out/Input_Parameters.txt")
     df = gen_panda(list, header)
+    #times = get_sun(list_params)
+    #print("Sunrise: {}\nSunset: {}".format(times["Sunrise"], times["Sunset"]))
     search_asts(df, params)
-    #df.to_csv("Asteroid_List.csv", index=False)
-    #write_list_data("Asteroid_List.txt", list_data)
+
+
 
 if __name__ == "__main__":
     main()
