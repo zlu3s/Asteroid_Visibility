18a6b13f3fbfc2d5a5751e91ab87c81aad524a3b
Zeke DeLaughter <zeke.delaughter@gmail.com>
2025-03-28 21:41:33 -0700

    Added a sorting algorithm for air massf

--- Code/Asteroid_list.py
+++ Code/Asteroid_list.py
@@ -13,8 +13,10 @@ from skyfield.almanac import find_discrete, sunrise_sunset
 url = "https://ssd.jpl.nasa.gov/api/horizons.api"
 
 locations = {
-    "Tucson": Topos(latitude_degrees=32.2226, longitude_degrees=-110.9747),
-    "UKIRT": Topos(latitude_degrees=19.825, longitude_degrees=-155.4717, elevation_m=4192)
+    #Tucson
+    "G37": Topos(latitude_degrees=32.2226, longitude_degrees=-110.9747),
+    #UKIRT
+    "V38": Topos(latitude_degrees=19.825, longitude_degrees=-155.4717, elevation_m=4192)
 }
 
 
@@ -108,8 +110,7 @@ def indiv_params(input):
         parameters["CENTER"] = "'G37'"
     return parameters
 
-
-
+"""
 def search_asts(df, params):
     print("There are {} asteroids in the list".format(len(df)))
     
@@ -117,8 +118,10 @@ def search_asts(df, params):
     
     while True:
         for i, desig in enumerate(df["Primary Desig"].head(count)):
-            ast = Asteroid(desig, df["Name"].iloc[i])  # Use iloc for indexing
+            name = df["Name"].iloc[i]
+            ast = Asteroid(desig, name) 
             ast.set_params(params)
+            print("Gathering data for {}...".format(name))
             response = requests.get(url, data=ast.params)
             data = response.json()
             if "\nNo site matches." in data['result']:
@@ -128,17 +131,41 @@ def search_asts(df, params):
                 ast.get_header(data)
                 ast.set_df()  
                 print(ast.df)
+        df = df.iloc[count:]
         answer = input("Would you like to observe more asteroid info? (Y/N): ").strip().upper()
         if answer == "Y":
             more = input("How many more asteroids would you like to observe: ")
             if more.isdigit():  # Ensure valid input
-                count += int(more)
+                count = int(more)
             else:
                 print("Invalid input, exiting.")
                 break
         else:
             break
+"""
 
+def sort_ast(df, params):
+    print("Begin Asteroid Sorting Process. There are {} total asteroids in the list.".format(len(df)))
+    valid_ast = {}
+    for i, desig in enumerate(df["Primary Desig"]):
+        name = df["Name"].iloc[i]
+        ast = Asteroid(desig, name) 
+        ast.set_params(params)
+        print("{}: Gathering data for {}...".format(i, name))
+        response = requests.get(url, data=ast.params)
+        data = response.json()
+        ast.get_ephem(data)
+        ast.get_header(data)
+        ast.set_df()
+        
+        add = True
+        # Sort for airmass
+        for index, value in ast.df['a-mass'].items():
+            if value == "n.a.":
+                add = False
+                print("Does not constrain to air mass")
+        if add: valid_ast[ast.desig] = ast.df
+    return valid_ast
 
 
 def get_sun(params):
@@ -150,15 +177,16 @@ def get_sun(params):
     ts = load.timescale()
     rise_set = {}
 
-    # Define location (Tucson, AZ)
     place = params['CENTER'].strip("'")
     location = locations[place]
+    if place == "G37": noon = 19
+    elif place == "V38": noon = 22
 
     # Define time range
-    start_time = ts.utc(int(start[0]), int(start[1]), int(start[2]))
-    end_time = ts.utc(int(end[0]), int(end[1]), int(end[2]))
+    start_time = ts.utc(int(start[0]), int(start[1]), int(start[2]), noon)
+    end_time = ts.utc(int(end[0]), int(end[1]), int(end[2]), noon)
 
-    # Get the sunrise and sunset function
+    # Get the sunrise and sunset functions
     f = sunrise_sunset(eph, location)
 
     # Find discrete events (sunrise, sunset) within the time range
@@ -169,11 +197,17 @@ def get_sun(params):
         event_name = "Sunrise" if e == 1 else "Sunset"
         rise_set[event_name] = t.utc_datetime()
 
-    params["START_TIME"] = "'{} {}'".format(start, times["Sunrise"])
-    params["STOP_TIME"] = "'{} {}'".format(end, times["Sunset"])
+    rise_set["Sunrise"] = rise_set["Sunrise"].strftime("%Y-%m-%d %H:%M:%S")
+    rise_set["Sunset"] = rise_set["Sunset"].strftime("%Y-%m-%d %H:%M:%S")
+    params["START_TIME"] = "'{}'".format(rise_set["Sunset"])
+    params["STOP_TIME"] = "'{}'".format(rise_set["Sunrise"])
 
-    return rise_set, params
+    return params
 
+
+def print_valid_ast(valid_ast):
+    for k,v in valid_ast.items():
+        print("{}\n{}".format(k,v))
 
 
 def main():
@@ -186,9 +220,9 @@ def main():
     params = indiv_params("C:/Users/Research/Asteroid Project/in_out/Input_Parameters.txt")
     df = gen_panda(list, header)
 
-    times, adjusted_params = get_sun(params)
-    search_asts(df, adjusted_params)
-
+    adjusted_params = get_sun(params)
+    valid_ast = sort_ast(df, adjusted_params)
+    print(valid_ast)
 
 
 if __name__ == "__main__":
